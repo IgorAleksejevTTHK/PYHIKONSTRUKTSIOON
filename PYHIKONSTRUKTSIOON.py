b63ebc512c869093d83936f488971072a246d98e
tsumakovamilana <tsumakovamilana955@gmail.com>
2025-01-20 11:25:08 +0200

    aga

--- PYHIKONSTRUKTSIOON.py
+++ PYHIKONSTRUKTSIOON.py
@@ -1,1 +1,31 @@
 ﻿#sep заменяет пробел между текстом sep="..."
+print()
+a=5
+aste=2
+#korrutamine
+tulemus=arv1*arv2
+arv1=int(input("esimene korrutaja: "))
+arv2=int(input("teine korrutaja: "))
+print(arv1,"*",arv2,"=",tulemus)
+#jagamine
+tulemus3=arv1/arv2
+arv1=int(input("esimene korrutaja: "))
+arv2=int(input("jagaja: "))
+print(arv1,"/",arv2,"=",tulemus)
+print(type(tulemus))
+
+arv3=4
+arv4=3
+tulemus2=arv3*arv4
+print(arv3,"*",arv4;"=",tulemus2)
+
+arv6=5
+arv5=8
+tulemus3=arv5-arv6
+print(arv3,"-",arv4;"=",tulemus3)
+
+arv8=7
+arv7=6
+tulemus4=arv7+arv8
+print(arv7,"+",arv8;"=",tulemus4)
+
