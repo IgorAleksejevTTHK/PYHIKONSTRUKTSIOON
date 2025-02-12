abfd62806e5f789be34b67db5480e3f521beaaed
IgorAleksejevTTHK <igoralekseje@gmail.com>
2025-02-12 15:46:55 +0200

    Update ifesle.py

--- ifesle.py
+++ ifesle.py
@@ -48,3 +48,24 @@ if vanus_input.isdigit():
         print("vanus ei saa olla negatiivne ega rohkem kui 100.")
 else:
     print("sisesta palun kehtiv taisarv.")
+
+
+
+
+
+def kas_nimi_kehtib(nimi):
+    return nimi.isalpha()
+
+
+nimi1 = input("sisesta esimene nimi: ")
+nimi2 = input("sisesta teine nimi: ")
+
+
+if kas_nimi_kehtib(nimi1) and kas_nimi_kehtib(nimi2):
+  
+    if (nimi1 == "igor" and nimi2 == "nikita") or (nimi1 == "igor" and nimi2 == "nikita"):
+        print("nikita ja igor on tana pinginaabrid")
+    else:
+        print("Need inimesed ei ole tana pinginaabrid")
+else:
+    print("Palun sisesta nimed, mis sisaldavad ainult tahti")
