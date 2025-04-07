1ab5a446d8bfc69fbfc39b80e0a6c0191b94d68d
IgorAleksejevTTHK <igoralekseje@gmail.com>
2025-04-07 15:44:42 +0300

    Update palgad_aleksejev.py

--- palgad-aleksejev/palgad_aleksejev.py
+++ palgad-aleksejev/palgad_aleksejev.py
@@ -1,4 +1,4 @@
-﻿from module1 import *
+from module1 import *
 
 palgad = [1200, 2500, 750, 395, 1200]
 inimesed = ["A", "B", "C", "D", "E"]
@@ -13,12 +13,12 @@ def menu():
         print("5. Palka sorteerimine")
         print("6. Võrdsed palgad")
         print("7. Otsi palka nimist")
-        print("8. otsib kes saavad rohkem/vähem kui määratud summa.")
-        print("9. otsib  vaeseimad ja rikkamad inimesed")
-
-        print("Välju")
+        print("8. Otsib kes saavad rohkem/vähem kui määratud summa.")
+        print("9. Otsib  vaeseimad ja rikkamad inimesed")
+        print("10. Keskmine palk")
+        print("11. Välju")
         
-        valik = input("Vali tegevus (1-9): ")
+        valik = input("Vali tegevus (1-11): ")
         
         if valik == "1":
             lisa_andmed(palgad, inimesed)
@@ -38,6 +38,9 @@ def menu():
             print(filtreeri_palgad(inimesed, palgad))
         elif valik == "9":
             print(top1(inimesed,palgad))
+        elif valik == "10":
+            print(keskmine_palk(inimesed, palgad))
+        elif valik == "11":
             break
         else:
             print("Vale valik, proovi uuesti.")
